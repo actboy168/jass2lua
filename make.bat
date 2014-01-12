d7684ba7d1ed2e2077e3129053b4d217b123efa5
actboy168 <actboy168@gmail.com>
2014-01-12 14:58:20 +0800

    参数错误时也pause

--- make.bat
+++ make.bat
@@ -1,8 +1,7 @@
 @echo off
 if {%1}=={} (
     echo usage: make.bat xxx.w3x
-    exit /B 1
+) else (
+    "%~dp0build\lua.exe" "%~dp0src\jass2lua.lua" %1 "%~dp0\"
 )
-
-"%~dp0build\lua.exe" "%~dp0src\jass2lua.lua" %1 "%~dp0\"
 pause
