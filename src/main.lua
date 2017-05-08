f6881450e5b3f18e34825df74a7976cfd8b2ce98
最萌小汐 <sunyijiusyj@126.com>
2017-05-08 22:01:28 +0800

    整理代码

--- src/main.lua
+++ src/main.lua
@@ -6,27 +6,52 @@ end)()
 
 require 'filesystem'
 require 'utility'
-local uni  = require 'unicode'
-local parser = require 'parser'
+local uni       = require 'unicode'
+local parser    = require 'parser'
 local converter = require 'converter'
+local archive   = require 'archive'
 
-local function main()
+local function convert_blizzard()
     local root = fs.path(uni.a2u(exepath)):parent_path():parent_path()
     local common   = io.load(root / 'src' / 'jass' / 'common.j')
     local blizzard = io.load(root / 'src' / 'jass' / 'blizzard.j')
+    local ast = parser(common, blizzard)
+    local buf = converter(ast)
+    io.save(root / 'blizzard.lua', buf)
+end
 
-    if not arg[1] then
-        local ast = parser(common, blizzard)
-        local buf = converter(ast)
-        io.save(root / 'blizzard.lua', buf)
-        print('完成,用时', os.clock(), '秒')
-        return
-    end
-    
-    local war3map = io.load(fs.path(uni.a2u(arg[1])))
+local function convert_war3map(path)
+    local root = fs.path(uni.a2u(exepath)):parent_path():parent_path()
+    local common   = io.load(root / 'src' / 'jass' / 'common.j')
+    local blizzard = io.load(root / 'src' / 'jass' / 'blizzard.j')
+    local war3map = io.load(path)
     local ast = parser(common, blizzard, war3map)
     local buf = converter(ast)
     io.save(root / 'war3map.lua', buf)
+end
+
+local function convert_map(path)
+    local map = archive(path, 'read')
+    if not map then
+        print('地图打开失败')
+        return
+    end
+    local listfile = map:load_file '(listfile)'
+    local files = {}
+end
+
+local function main()
+    if not arg[1] then
+        convert_blizzard()
+    else
+        local path = fs.path(uni.a2u(arg[1]))
+        if path:filename():string() == 'war3map.j' then
+            convert_war3map(path)
+        else
+            convert_map(path)
+        end
+    end
+    
     print('完成,用时', os.clock(), '秒')
 end
 
