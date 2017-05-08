f594b23fa44dcf2ffbc9091e9e3220e9ddfded03
最萌小汐 <sunyijiusyj@126.com>
2017-05-08 21:17:03 +0800

    整理代码

--- src/main.lua
+++ src/main.lua
@@ -12,20 +12,20 @@ local converter = require 'converter'
 
 local function main()
     local root = fs.path(uni.a2u(exepath)):parent_path():parent_path()
-    print(root)
-    parser:init(root)
+    local common   = io.load(root / 'src' / 'jass' / 'common.j')
+    local blizzard = io.load(root / 'src' / 'jass' / 'blizzard.j')
 
     if not arg[1] then
-        local result = parser()
-        local buf = converter(result)
+        local ast = parser(common, blizzard)
+        local buf = converter(ast)
         io.save(root / 'blizzard.lua', buf)
         print('完成,用时', os.clock(), '秒')
         return
     end
     
-    local jass = io.load(fs.path(uni.a2u(arg[1])))
-    local result = parser(jass)
-    local buf = converter(result)
+    local war3map = io.load(fs.path(uni.a2u(arg[1])))
+    local ast = parser(common, blizzard, war3map)
+    local buf = converter(ast)
     io.save(root / 'war3map.lua', buf)
     print('完成,用时', os.clock(), '秒')
 end
